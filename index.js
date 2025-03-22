6535093c2fd3e4675532ffd2b220035031c1cb55
ruzicjakov <jbenedikt1011@gmail.com>
2025-03-22 15:10:55 +0100

    Setting up Express.js server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, error => {
+    if (error) {
+        console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+        console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    }
+});
